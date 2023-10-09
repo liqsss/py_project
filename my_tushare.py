aee4075d93894bf74c0485d7ebdd17f6dab2c70b
liq <meyers_007@sohu.com>
2023-10-09 15:54:16 +0800

    fix bug

--- my_tushare.py
+++ my_tushare.py
@@ -40,6 +40,8 @@ sz = pro.daily(**{
 ])
 
 plt.plot(df.index, df['close'], label="Line 1")
+plt.yscale('linear')
+#plt.plot(df['trade_date'], df['close'], label="Line 1")
 plt.legend()
 plt.show()
 
